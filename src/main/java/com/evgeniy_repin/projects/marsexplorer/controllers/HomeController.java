3a045177fea12fbac74e2c8bdb5cb8071756f6ca
Evgeniy Repin <erepin64@gmail.com>
2022-01-11 22:47:56 +0800

    configure many files

--- src/main/java/com/evgeniy_repin/projects/marsexplorer/controllers/HomeController.java
+++ src/main/java/com/evgeniy_repin/projects/marsexplorer/controllers/HomeController.java
@@ -1,4 +1,17 @@
 package com.evgeniy_repin.projects.marsexplorer.controllers;
 
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String getHomeView(Model model) {
+        model.addAttribute("name", "Eugene");
+        return "home_view";
+    }
 }
